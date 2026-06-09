4992c344a838f6523cbe5c7977dd91fd0cd2b4cc
rukshanaroshan <rukshanaroshan.it@gmail.com>
2026-06-09 19:31:53 +0530

    adding html and js file

---
+++ 14.1+Javascript+alert/script.js
@@ -0,0 +1,13 @@
+// alert("Hello!");
+// alert("World!");
+
+// alert(2+3);
+
+ var myname="Rukshana";
+// alert(myname);
+var name=prompt("What is your name?");
+
+alert("My name is : "+ myname +" "+"Welcome to my channel" +" "+myname);
+
+
+
