e4ee33d0071638cf7a1d74e016bf62a81139b8bd
rukshanaroshan <rukshanaroshan.it@gmail.com>
2026-06-19 10:00:48 +0530

    pass or fail file adding

---
+++ my-task/01.Java-operators-and-control-flow-statements/PassOrFail.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PassOrFail {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input marks
+        System.out.print("Enter student marks: ");
+        int marks = input.nextInt();
+
+        // Check pass or fail
+        if (marks >= 40) {
+            System.out.println("Result: Pass");
+        } else if (marks >= 0) {
+            System.out.println("Result: Fail");
+        } else {
+            System.out.println("Invalid marks!");
+        }
+
+        input.close();
+    }
+}
